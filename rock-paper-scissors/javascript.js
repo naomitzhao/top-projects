c533f081361d96480c43c3f7107ebc569197cfb5
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-04-08 14:52:55 -0700

    getComputerChoice and rockPaperScissors helper functions

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,5 +1,52 @@
-console.log("hello world");
-
 function getComputerChoice() {
-    return "hello";
+    var roll = Math.floor(Math.random() * 3);
+    if (roll == 0){
+        return "rock";
+    }
+    else if (roll == 1) {
+        return "paper"
+    }
+    else if (roll == 2) {
+        return "scissors"
+    }
 }
+
+function rockPaperScissors(playerSelection, computerSelection) {
+    var choice = playerSelection.toLowerCase();
+    if (choice == computerSelection) {
+        console.log("tie! you both chose " + computerSelection);
+    }
+    else if (choice == "rock") {
+        if (computerSelection == "paper") {
+            console.log("paper beats rock, you lose")
+            
+        }
+        else {
+            console.log("rock beats scissors, you win")
+        }
+    }
+    else if (choice == "paper") {
+        if (computerSelection == "rock") {
+            console.log("paper beats rock, you win")
+        }
+        else {
+            console.log("scissors beats paper, you lose")
+        }
+    }
+    else {
+        if (computerSelection == "rock") {
+            console.log("rock beats scissors, you lose")
+        }
+        else {
+            console.log("scissors beats paper, you win")
+        }
+    }
+}
+
+function game() {
+    console.log("welcome to rock paper scissors! what is your first move? (type 'rock', 'paper', or 'scissors')")
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
