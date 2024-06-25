8698ce36090a4bbe19517e3ed1cc66410d52ee3f
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-06-25 15:34:55 -0700

    refactored basic layout generation

--- calculator/javascript.js
+++ calculator/javascript.js
@@ -15,20 +15,31 @@ function operate(a, b, operator) {
 
 console.log(operate(18, 6, "/"));
 
-const buttonRows = document.querySelectorAll(".buttonRow");
-buttonRows.forEach((buttonRow) => {
-    for (i = 0; i < 4; i ++) {
-        const newButton = document.createElement("div");
-        newButton.classList.add("calculatorButton");
-        newButton.addEventListener("mousedown", () => {
-            newButton.classList.add("activeButton");
+const buttonDiv = document.querySelector(".buttonDiv");
+
+let rowIdx = 0;
+let layoutIdx = 0;
+const layout = ["AC", "√", "<", "+/-", "7", "8", "9", "/", "4", "5", "6", "x", "1", "2", "3", "-", "0", ".", "=", "+"]
+
+for (i = 0; i < 5; i ++) {
+    const buttonRow = document.createElement("div");
+    buttonRow.classList.add("buttonRow");
+    for (j = 0; j < 4; j ++) {
+        const btn = document.createElement("div");
+        btn.classList.add("calculatorButton");
+        btn.addEventListener("mousedown", () => {
+            btn.classList.add("activeButton");
         });
-        newButton.addEventListener("mouseup", () => {
-            newButton.classList.remove("activeButton");
+        btn.addEventListener("mouseup", () => {
+            btn.classList.remove("activeButton");
         });
-        newButton.addEventListener("mouseleave", () => {
-            newButton.classList.remove("activeButton");
+        btn.addEventListener("mouseleave", () => {
+            btn.classList.remove("activeButton");
         });
-        buttonRow.appendChild(newButton);
+        btn.id = layout[layoutIdx];
+        btn.textContent = layout[layoutIdx];
+        layoutIdx ++;
+        buttonRow.appendChild(btn);
     }
-});
+    buttonDiv.appendChild(buttonRow);
+}
