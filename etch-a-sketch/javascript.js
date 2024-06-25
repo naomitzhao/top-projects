5bbd05f319e35a72d57975a956f199c0df773d79
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-06-24 18:57:51 -0700

    added ability to generate grids of different dimensions

--- etch-a-sketch/javascript.js
+++ etch-a-sketch/javascript.js
@@ -1,15 +1,44 @@
 const sketchContainer = document.querySelector("#sketchContainer");
 
-for (i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("tileRow");
-    sketchContainer.appendChild(row);
-    for (j = 0; j < 16; j++) {
-        const tile = document.createElement("div");
-        tile.classList.add("tile");
-        row.appendChild(tile);
-        tile.addEventListener("mouseover", () => {
-            tile.classList.add("inkedTile");
-        });
+function makeNewGrid(dim) {
+    const tiles = document.querySelectorAll(".tile");
+    tiles.forEach((t) => {
+        t.remove();
+    });
+    const rows = document.querySelectorAll(".tileRow");
+    rows.forEach((r) => {
+        r.remove();
+    });
+
+    tileWidth = "calc(1/" + dim + " * 100%)";
+
+    for (i = 0; i < dim; i++) {
+        const row = document.createElement("div");
+        row.classList.add("tileRow");
+        sketchContainer.appendChild(row);
+        for (j = 0; j < dim; j++) {
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.style.width = tileWidth;
+            row.appendChild(tile);
+            tile.addEventListener("mouseover", () => {
+                tile.classList.add("inkedTile");
+            });
+        }
     }
 }
+
+function clearGrid(){
+    const tiles = document.querySelectorAll(".tile");
+    tiles.forEach((t) => {
+        t.classList.remove("inked");
+    });
+}
+
+const newGridButton = document.querySelector('#newGridButton');
+newGridButton.addEventListener("click", () => {
+    newGridSize = prompt("type the dimension of your new grid (how many squares on one side)");
+    makeNewGrid(newGridSize);
+});
+
+makeNewGrid(16);
