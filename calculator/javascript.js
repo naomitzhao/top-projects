eca4a032422077dc66956c6cc5bd0fc0e0e70a28
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-06-25 14:01:23 -0700

    basic layout

--- calculator/javascript.js
+++ calculator/javascript.js
@@ -14,3 +14,12 @@ function operate(a, b, operator) {
 }
 
 console.log(operate(18, 6, "/"));
+
+const buttonRows = document.querySelectorAll(".buttonRow");
+buttonRows.forEach((buttonRow) => {
+    for (i = 0; i < 4; i ++) {
+        const newButton = document.createElement("div");
+        newButton.classList.add("calculatorButton");
+        buttonRow.appendChild(newButton);
+    }
+});
