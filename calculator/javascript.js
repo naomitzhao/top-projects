42d15e5f29f5bbaedd61dcc243739e54b89fd9ed
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-06-25 14:13:56 -0700

    click button animation

--- calculator/javascript.js
+++ calculator/javascript.js
@@ -20,6 +20,15 @@ buttonRows.forEach((buttonRow) => {
     for (i = 0; i < 4; i ++) {
         const newButton = document.createElement("div");
         newButton.classList.add("calculatorButton");
+        newButton.addEventListener("mousedown", () => {
+            newButton.classList.add("activeButton");
+        });
+        newButton.addEventListener("mouseup", () => {
+            newButton.classList.remove("activeButton");
+        });
+        newButton.addEventListener("mouseleave", () => {
+            newButton.classList.remove("activeButton");
+        });
         buttonRow.appendChild(newButton);
     }
 });
