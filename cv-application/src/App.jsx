a13015c99663486a3396a73cfb6dfbd9e34a5f07
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-07-25 15:24:43 -0700

    react app init

---
+++ cv-application/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log("hello world :3");
+
+  return (
+    <>
+      <div className="form">hi</div>
+      <div className="resumeContainer">hello</div>
+    </>
+  )
+}
+
+export default App
