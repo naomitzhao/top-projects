26e2a70b70bfd0976c11019ba13c72af843600b9
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-08-05 14:07:06 -0700

    refactor: use IIFEs only

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,13 +1,6 @@
 import './styles.css';
 import Plus from './assets/plus.svg';
 
-let idx = 0;
-let currentGroup = "ungrouped";
-let newCategoryOpen = false;
-let currTodoEdit = null;
-
-const todos = [];
-
 const categories = (function () {
     const map = new Map();
 
@@ -37,6 +30,10 @@ const categories = (function () {
 }) ();
 
 const domStuff = (function () {
+    let newCategoryOpen = false;
+
+    const content = document.getElementById("content")
+
     const addTodo = function (todo) {
         const item = document.createElement("div");
         item.classList.add("item");
@@ -83,8 +80,8 @@ const domStuff = (function () {
         });
     
         h5.addEventListener("click", () => {
-            currTodoEdit = todo;
-            showDialog();
+            todoList.currTodoEdit = todo;
+            domStuff.showDialog();
         });
     };
 
@@ -115,12 +112,12 @@ const domStuff = (function () {
     
                 name.addEventListener("keydown", (e) => {
                     if (e.key == 'Enter') {
-                        addCategory(name.value, newCategoryDiv);
+                        todoList.addCategory(name.value, newCategoryDiv);
                     }
                 });
     
                 btn.addEventListener("click", () => {
-                    addCategory(name.value, newCategoryDiv);
+                    todoList.addCategory(name.value, newCategoryDiv);
                 });
     
                 cancelBtn.addEventListener("click", () => {
@@ -130,7 +127,7 @@ const domStuff = (function () {
         });
     };
 
-    const addCategory = function (category, newCategoryDiv) {
+    const addCategory = function (category) {
         if (category != '') {
             const nav = document.querySelector("nav");
             const btn = document.createElement("button");
@@ -141,8 +138,6 @@ const domStuff = (function () {
             });
         
             nav.appendChild(btn);
-            
-            categories.addCategory(category, newCategoryDiv);
         }
         closeAddCategory();
     };
@@ -153,216 +148,218 @@ const domStuff = (function () {
         newCategoryDiv.remove();
     };
 
-    return { addTodo, loadAddCategory, addCategory };
+    const switchTab = function (category) {
+        content.querySelector("h2").textContent = category;
+        content.removeChild(document.getElementById("tasks"));
+        content.append(categories.get(category));
+        currentGroup = category;
+    };
+
+    const editTodo = function (todo, oldCategory) {
+        const item = document.getElementById("item" + todo.id);
+        
+        const priorityBar = item.querySelector(".priorityBar");
+        priorityBar.style.backgroundColor = `var(--priority-${todo.priority})`;
+        
+        const h5 = item.querySelector("h5");
+        h5.textContent = todo.title;
+    
+        const dateP = item.querySelector("p");
+        dateP.textContent = todo.date;
+    
+        if (todo.category != oldCategory) {
+            categories.keys().forEach((key) => {
+                if (key == oldCategory) {
+                    categories.get(key).removeChild(item);
+                }
+                else if (key == todo.category) {
+                    categories.get(key).appendChild(item);
+                }
+            });
+        }
+    };
+
+    const loadAddTaskCategories = function () {
+        const categoryDropDown = document.createElement("select");
+        categoryDropDown.classList.add("addTaskInput");
+        categoryDropDown.name = "category";
+        for (let category of categories.keys()){
+            const option = document.createElement("option");
+            option.value = category;
+            option.textContent = category;
+            if (category == currentGroup) {
+                option.selected = true;
+            }
+    
+            categoryDropDown.appendChild(option);
+        }
+        return categoryDropDown;
+    };
+
+    const showDialog = function () {
+        const dialogContainer = document.getElementById("dialogContainer");
+        dialogContainer.style.display = "flex";
+        const dialog = document.querySelector("dialog");
+        dialog.show();
+    
+        const categoryField = document.getElementById("categoryField");
+        const categoryDropDown = categoryField.querySelector("select");
+        categoryDropDown.remove();
+    
+        categoryField.appendChild(loadAddTaskCategories());
+    
+        if (todoList.currTodoEdit != null) {
+            document.getElementById("titleInput").value = todoList.currTodoEdit.title;
+            document.getElementById("dateInput").value = todoList.currTodoEdit.date;
+            document.getElementById("descriptionInput").value = todoList.currTodoEdit.description;
+        }
+    };
+    
+    const hideDialog = function () {
+        const dialogContainer = document.getElementById("dialogContainer");
+        dialogContainer.style.display = "none";
+        const dialog = document.querySelector("dialog");
+        clearDialog(document.getElementById("addTaskForm"));
+        dialog.close();
+    };
+    
+    const clearDialog = function (form) {
+        form.querySelectorAll("input, textarea").forEach((field) => {
+            field.value = "";
+        });
+        todoList.currTodoEdit = null;
+    };
+
+    const getFormData = function () {
+        const addTaskForm = document.getElementById("addTaskForm");
+
+        const title = addTaskForm.elements["title"].value;
+        const date = addTaskForm.elements["date"].value;
+        const priority = addTaskForm.elements["priority"].value;
+        const category = addTaskForm.elements["category"].value;
+        const description = addTaskForm.elements["description"].value;
+
+        domStuff.hideDialog();
+
+        return { title, date, priority, category, description };
+    };
+
+    content.append(categories.get("ungrouped"));
+
+    return { newCategoryOpen, addTodo, loadAddCategory, addCategory, switchTab, editTodo, showDialog, hideDialog, clearDialog, getFormData };
 })();
 
 const todoList = (function () {
-    const addCategory = function (name, inputDiv) {
+    const todos = [];
+    let currTodoEdit = null;
+    let idx = 0;
+
+    const addCategory = function (name, newCategoryDiv) {
         if (name) {
             categories.addCategory(name);
             domStuff.addCategory(name);
-            switchTab(name);
+            domStuff.switchTab(name);
         }
-        inputDiv.remove();
+        newCategoryDiv.remove();
     
-        newCategoryOpen = false;
+        domStuff.newCategoryOpen = false;
     };
 
-    return { addCategory }
+    const addTodo = function (title, date, priority, category, description = ""){
+        const todo = makeTodo(title, date, priority, category, description);
+        todos.push(todo);
+        domStuff.addTodo(todo);
+    };
+    
+    const editTodo = function (id, title, date, priority, category, description = "") {
+        todos.forEach((todo) => {
+            if (todo.id == id) {
+                const oldCategory = todo.category;
+                todo.title = title;
+                todo.date = date;
+                todo.priority = priority;
+                todo.category = category;
+                todo.description = description;
+                domStuff.editTodo(todo, oldCategory);
+                return;
+            }
+        });
+    };
+
+    const makeTodo = function (title, date, priority, category, description) {
+        return {
+            title: title,
+            date: date,
+            priority: priority, 
+            category: category, 
+            description: description,
+            complete: false, 
+            id: idx++
+        };
+    };
+
+    const handleAddTask = function () {
+        const data = domStuff.getFormData();
+        addTodo(data.title, data.date, data.priority, data.category, data.description);
+    };
+
+    const handleEditTask = function (taskId) {
+        const data = domStuff.getFormData()
+        editTodo(taskId, data.title, data.date, data.priority, data.category, data.description);
+    };
+
+    return { currTodoEdit, addCategory, addTodo, editTodo, handleAddTask, handleEditTask }
 })();
 
-loadAddTask();
-populateNav();
+const init = (function () {
+    const loadAddTask = function () {
+        const addTask = document.getElementById("addTask");
+        addTask.querySelector("img").src = Plus;
+        const dialogOverlay = document.getElementById("dialogOverlay");
+        const addTaskButton = document.getElementById("addTaskButton")
+    
+        dialogOverlay.addEventListener("click", () => {
+            domStuff.hideDialog();
+        });
+    
+        addTask.addEventListener("click", () => {
+            domStuff.showDialog();
+        });
+    
+        addTaskButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (currTodoEdit != null) {
+                todoList.handleEditTask(currTodoEdit.id);
+            }
+            else {
+                todoList.handleAddTask();
+            }
+        });
+    };
+
+    const loadNav = function () {
+        const nav = document.querySelector("nav");
+    
+        for (let category of categories.keys()) {
+            const btn = document.createElement("button");
+            btn.textContent = category;
+    
+            btn.addEventListener("click", () => {
+                domStuff.switchTab(category);
+            });
+    
+            nav.appendChild(btn);
+        }
+    };
+
+    return { loadAddTask, loadNav }
+})();
+
+init.loadAddTask();
+init.loadNav();
 domStuff.loadAddCategory();
 
-const content = document.getElementById("content")
-
-addTodo("test todo", "thu 7/25", 0, "grouped", "hello hello!");
-addTodo("walk gerald", "fri 7/26", 2, "ungrouped", "take gerald for a walk");
-addTodo("water the sink", "sat 7/27", 0, "ungrouped", "the sink is thirsty");
-addTodo("do something cool", "sun 7/29", 1, "grouped", "what is cool?");
-
-content.append(categories.get("ungrouped"));
-
-function loadAddTask () {
-    const addTask = document.getElementById("addTask");
-    addTask.querySelector("img").src = Plus;
-    const dialogOverlay = document.getElementById("dialogOverlay");
-    const addTaskButton = document.getElementById("addTaskButton")
-
-    dialogOverlay.addEventListener("click", () => {
-        hideDialog();
-    });
-
-    addTask.addEventListener("click", () => {
-        showDialog();
-    });
-
-    addTaskButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        if (currTodoEdit != null) {
-            handleEditTask(currTodoEdit.id);
-        }
-        else {
-            handleAddTask();
-        }
-    });
-}
-
-function handleAddTask() {
-    const addTaskForm = document.getElementById("addTaskForm");
-
-    const title = addTaskForm.elements["title"].value;
-    const date = addTaskForm.elements["date"].value;
-    const priority = addTaskForm.elements["priority"].value;
-    const category = addTaskForm.elements["category"].value;
-    const description = addTaskForm.elements["description"].value;
-
-    addTodo(title, date, priority, category, description);
-
-    hideDialog();
-}
-
-function handleEditTask(taskId) {
-    const addTaskForm = document.getElementById("addTaskForm");
-
-    const title = addTaskForm.elements["title"].value;
-    const date = addTaskForm.elements["date"].value;
-    const priority = addTaskForm.elements["priority"].value;
-    const category = addTaskForm.elements["category"].value;
-    const description = addTaskForm.elements["description"].value;
-
-    editTodo(taskId, title, date, priority, category, description);
-
-    hideDialog();
-}
-
-function loadAddTaskCategories () {
-    const categoryDropDown = document.createElement("select");
-    categoryDropDown.classList.add("addTaskInput");
-    categoryDropDown.name = "category";
-    for (let category of categories.keys()){
-        const option = document.createElement("option");
-        option.value = category;
-        option.textContent = category;
-        if (category == currentGroup) {
-            option.selected = true;
-        }
-
-        categoryDropDown.appendChild(option);
-    }
-    return categoryDropDown;
-}
-
-function populateNav() {
-    const nav = document.querySelector("nav");
-
-    for (let category of categories.keys()) {
-        const btn = document.createElement("button");
-        btn.textContent = category;
-
-        btn.addEventListener("click", () => {
-            switchTab(category);
-        });
-
-        nav.appendChild(btn);
-    }
-}
-
-function switchTab(category) {
-    content.querySelector("h2").textContent = category;
-    content.removeChild(document.getElementById("tasks"));
-    content.append(categories.get(category));
-    currentGroup = category;
-}
-
-function addTodo(title, date, priority, category, description = ""){
-    const todo = makeTodo(title, date, priority, category, description);
-    todos.push(todo);
-    domStuff.addTodo(todo);
-}
-
-function editTodo(id, title, date, priority, category, description = "") {
-    todos.forEach((todo) => {
-        if (todo.id == id) {
-            const oldCategory = todo.category;
-            todo.title = title;
-            todo.date = date;
-            todo.priority = priority;
-            todo.category = category;
-            todo.description = description;
-            editDomTodo(todo, oldCategory);
-            return;
-        }
-    });
-}
-
-function makeTodo(title, date, priority, category, description) {
-    return {
-        title: title,
-        date: date,
-        priority: priority, 
-        category: category, 
-        description: description,
-        complete: false, 
-        id: idx++
-    };
-}
-
-function editDomTodo(todo, oldCategory) {
-    const item = document.getElementById("item" + todo.id);
-    
-    const priorityBar = item.querySelector(".priorityBar");
-    priorityBar.style.backgroundColor = `var(--priority-${todo.priority})`;
-    
-    const h5 = item.querySelector("h5");
-    h5.textContent = todo.title;
-
-    const dateP = item.querySelector("p");
-    dateP.textContent = todo.date;
-
-    if (todo.category != oldCategory) {
-        categories.keys().forEach((key) => {
-            if (key == oldCategory) {
-                categories.get(key).removeChild(item);
-            }
-            else if (key == todo.category) {
-                categories.get(key).appendChild(item);
-            }
-        });
-    }
-}
-
-function showDialog() {
-    const dialogContainer = document.getElementById("dialogContainer");
-    dialogContainer.style.display = "flex";
-    const dialog = document.querySelector("dialog");
-    dialog.show();
-
-    const categoryField = document.getElementById("categoryField");
-    const categoryDropDown = categoryField.querySelector("select");
-    categoryDropDown.remove();
-
-    categoryField.appendChild(loadAddTaskCategories());
-
-    if (currTodoEdit != null) {
-        document.getElementById("titleInput").value = currTodoEdit.title;
-        document.getElementById("dateInput").value = currTodoEdit.date;
-        document.getElementById("descriptionInput").value = currTodoEdit.description;
-    }
-}
-
-function hideDialog() {
-    const dialogContainer = document.getElementById("dialogContainer");
-    dialogContainer.style.display = "none";
-    const dialog = document.querySelector("dialog");
-    clearDialog(document.getElementById("addTaskForm"));
-    dialog.close();
-}
-
-function clearDialog(form) {
-    form.querySelectorAll("input, textarea").forEach((field) => {
-        field.value = "";
-    });
-    currTodoEdit = null;
-}
+todoList.addTodo("test todo", "thu 7/25", 0, "grouped", "hello hello!");
+todoList.addTodo("walk gerald", "fri 7/26", 2, "ungrouped", "take gerald for a walk");
+todoList.addTodo("water the sink", "sat 7/27", 0, "ungrouped", "the sink is thirsty");
+todoList.addTodo("do something cool", "sun 7/29", 1, "grouped", "what is cool?");
