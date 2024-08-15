cbe49c55ceec4b78a9d4a76fbf3cc0c7e922b3cb
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-08-14 20:27:04 -0700

    feat: Add pin icon to indicate pinned messages

--- message-board/routes/indexRouter.js
+++ message-board/routes/indexRouter.js
@@ -7,12 +7,14 @@ const messages = [
       user: "naomi",
       added: new Date("2024-08-13T19:43:01"),
       color: 4, 
+      pinned: true,
     },
     {
       text: "this is the mini message board hehe! let's make this message a bit longer so you have to click it to read the full thing.",
       user: "naomi",
       added: new Date("2024-08-13T19:43:59"), 
       color: 5, 
+      pinned: true,
     }
   ];
 
@@ -29,7 +31,8 @@ indexRouter.post("/new", (req, res) => {
         text: req.body.message, 
         user: req.body.authorName, 
         added: new Date(), 
-        color: Math.floor(Math.random() * 6),
+        color: Math.floor(Math.random() * 5) + 1,
+        pinned: false,
     });
     res.redirect("/");
 });
